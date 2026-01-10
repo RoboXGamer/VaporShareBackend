cbac9c2b5d313430618467e49db2b4ba65c95e09
RoboXGamer <roboxgamer52@gmail.com>
2026-01-10 18:33:55 +0530

    use path alias

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { app } from "./app";
-import { connectDB } from "./db";
+import { app } from "@/app";
+import { connectDB } from "@/db";
 
 async function main() {
   try {
     await connectDB();
     app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+      console.log(`Server is running!! on port ${process.env.PORT}`);
     });
   } catch (error) {
     console.log("Error connecting to DB", error);
