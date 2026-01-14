fc489ff39bbaae1b29411ccbb3ffd1df57094e44
RoboXGamer <roboxgamer52@gmail.com>
2026-01-14 17:37:50 +0530

    feat: bootstrap express app with middleware, routing, and database connection

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,51 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { User } from "./models/user.model";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import logger from "./utils/logger";
+//routes import
+import userRouter from "./routes/user";
+import { ApiResponse } from "./utils/ApiResponse";
+import { errorHandler } from "./middlewares/error.middleware";
 
 dotenv.config();
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+const morganFormat =
+  ":method :url :status :res[content-length] - :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  }),
+);
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.header("test", "test");
-  res.send("World!");
+  res.send(
+    new ApiResponse(200, {
+      message: "VaporShare API | Server is running",
+    }),
+  );
 });
 
-app.get("/test", (req, res) => {
-  let name = "Prakhar";
-  res.send({ name });
-});
+app.use("/api/v1/users", userRouter);
 
-app.post("/register", async (req, res) => {
-  const { username, email } = req.body;
-  if (!username || !email) {
-    res.status(400).send({ message: "Username or email is required" });
-    return;
-  }
-  const user = new User({ username, email });
-  try {
-    await user.save();
-    res.send({ message: "User registered successfully" });
-  } catch (error) {
-    res.status(500).send({ message: "Error registering user", error });
-  }
-  console.log(user);
-});
+// Error Middleware
+app.use(errorHandler);
+
 export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { app } from "@/app";
 import { connectDB } from "@/db";
+import logger from "./utils/logger";
 
 async function main() {
   try {
     await connectDB();
     app.listen(process.env.PORT, () => {
-      console.log(`Server is running!! on port ${process.env.PORT}`);
+      logger.info(`Server is running on port ${process.env.PORT}`);
     });
   } catch (error) {
-    console.log("Error connecting to DB", error);
+    logger.error("Error connecting to DB", error);
   }
 }
 
