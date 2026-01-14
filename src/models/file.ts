33870ed2dd7f637e55516d86bafa3e3595735d3f
RoboXGamer <roboxgamer52@gmail.com>
2026-01-14 18:58:26 +0530

    add timestamps to the file model

--- src/models/file.ts
+++ src/models/file.ts
@@ -1,32 +1,35 @@
 import { model, Schema } from "mongoose";
 
-const fileSchema = new Schema({
-  filename: {
-    type: String,
-    required: true,
+const fileSchema = new Schema(
+  {
+    filename: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    key: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    expiry: {
+      type: Date,
+      required: true,
+    },
+    userid: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  key: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  expiry: {
-    type: Date,
-    required: true,
-  },
-  userid: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-});
+  { timestamps: true },
+);
 
 export const File = model("File", fileSchema);
