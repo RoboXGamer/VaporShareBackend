71dccd31f2371ce76503dbfae6959b62cc9c7f8e
PrakharBelwal <prakhar.belwal@gmail.com>
2026-01-18 01:55:18 +0530

    added encryption, compare, token methods

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
@@ -25,5 +27,35 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+// encrypting password before saving or modifying
+
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next()
+  
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password: string){
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.genrateAccessToken = function (){
+  return jwt.sign({
+    _id: this._id,
+    type: this.type
+  },process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  })
+}
+userSchema.methods.genrateRefreshToken = function (){
+  return jwt.sign({
+    _id: this._id,
+    type: this.type
+  },process.env.REFRESH_TOKEN_SECRET,{
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  })
+}
 
 export const User = mongoose.model("User", userSchema);
