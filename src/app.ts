4ef360b02aa3902738f287dea98c5c85dd30aa84
PrakharBelwal <prakhar.belwal@gmail.com>
2026-01-18 17:03:33 +0530

    added user controller and route

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,10 @@ app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes
+
+import userRouter from "./routes/user.routes";
+
+app.use("/api/v1/user", userRouter);
+
 export { app };
